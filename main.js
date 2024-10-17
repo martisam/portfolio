bee1290def62d156f97700bcd3401a16bfa8e82e
Martínez <samuel.hernandez@accenture.com>
2024-10-17 11:29:09 -0700

    bug fix

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ function addStar() {
 Array(200).fill().forEach(addStar);
 
 //space background
-const spaceTexture = new THREE.TextureLoader().load('space.jpeg');
+const spaceTexture = new THREE.TextureLoader().load('Universe.png');
 scene.background = spaceTexture;
 
 
