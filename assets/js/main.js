02d43739e42c5f190db431994e29846ee7d65a40
Martínez <samuel.hernandez@accenture.com>
2024-10-17 23:08:56 -0700

    UI updates

---
+++ assets/js/main.js
@@ -0,0 +1,135 @@
+
+import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
+
+//setting up background
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio( window.devicePixelRatio);
+renderer.setSize( window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
+
+renderer.render(scene, camera);
+
+//Creating the Donut/Torus geometry
+const geometry = new THREE.TorusGeometry( 10, 3, 15, 100);
+const material = new THREE.MeshBasicMaterial( { color: 0xFFFFFF, wireframe: true});
+const torus = new THREE.Mesh( geometry, material );
+
+scene.add(torus);
+
+//Setting the ligthning
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5,5,5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial( {color: 0xffffff});
+  const star = new THREE.Mesh( geometry, material);
+
+  const [x,y,z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x,y,z);
+  scene.add(star);
+}
+
+Array(200).fill().forEach(addStar);
+
+//space background
+/**
+const spaceTexture = new THREE.TextureLoader().load('space4k.jpg');
+scene.background = spaceTexture;
+
+
+*/
+
+//Profile
+const profTexture = new THREE.TextureLoader().load('assets/images/profile.jpg');
+
+const profile = new THREE.Mesh(
+  new THREE.BoxGeometry(3,3,3),
+  new THREE.MeshBasicMaterial( { map: profTexture})
+);
+
+scene.add(profile)
+
+profile.position.x = 2;
+profile.position.z = -5;
+
+//Animation Scroll
+function moveCamera() {
+
+  const t = document.body.getBoundingClientRect().top;
+  profile.rotation.y += 0.01;
+  profile.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.0002;
+
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+function animate(){
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  //controls.update();
+  renderer.render( scene, camera);
+}
+
+animate();
+
+var util = {
+  mobileMenu() {
+    $("#nav").toggleClass("nav-visible");
+  },
+  windowResize() {
+    if ($(window).width() > 800) {
+      $("#nav").removeClass("nav-visible");
+    }
+  },
+  scrollEvent() {
+    var scrollPosition = $(document).scrollTop();
+    
+    $.each(util.scrollMenuIds, function(i) {
+      var link = util.scrollMenuIds[i],
+          container = $(link).attr("href"),
+          containerOffset = $(container).offset().top,
+          containerHeight = $(container).outerHeight(),
+          containerBottom = containerOffset + containerHeight;
+
+      if (scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20) {
+        $(link).addClass("active");
+      } else {
+        $(link).removeClass("active");
+      }
+    });
+  }
+};
+
+$(document).ready(function() {
+  
+  util.scrollMenuIds = $("a.nav-link[href]");
+  $("#menu").click(util.mobileMenu);
+  $(window).resize(util.windowResize);
+  $(document).scroll(util.scrollEvent);
+  
+});
+
